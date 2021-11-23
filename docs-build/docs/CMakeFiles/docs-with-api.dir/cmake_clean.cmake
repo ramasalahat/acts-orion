572c86e20559bdc0775f0344caa188c613927376
Corentin Allaire <corentin.allaire@cern.ch>
2021-11-23 14:22:26 +0100

    git rm

--- docs-build/docs/CMakeFiles/docs-with-api.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "../../docs/_build/doxygen-xml/index.xml"
-  "CMakeFiles/docs-with-api"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/docs-with-api.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
