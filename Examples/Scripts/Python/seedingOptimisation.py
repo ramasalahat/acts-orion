daa14b5c24afd3fe49a616d381f76953bdb4b23a
ramasalahat <ramasalahat@gmail.com>
2022-05-16 10:25:42 +0200

    changing fidelity base

--- Examples/Scripts/Python/seedingOptimisation.py
+++ Examples/Scripts/Python/seedingOptimisation.py
@@ -316,7 +316,7 @@ if "__main__" == __name__:
         "radLengthPerSeed": "uniform(0.01, 0.1)",
         "compatSeedWeight": "uniform(100, 1000)",
         "impactWeightFactor": "uniform(0.5, 5)",
-        "events": "fidelity(low=10, high=1000, base=10)"
+        "events": "fidelity(low=10, high=1000)"
     }
 
     experiment = build_experiment(
