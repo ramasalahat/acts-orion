3c3c20e36fe5363290e887b9753c13d0b60f4e01
ramasalahat <ramasalahat@gmail.com>
2022-05-17 15:25:06 +0200

    changing the geoSelection to open data detector

--- Examples/Scripts/Python/seeding.py
+++ Examples/Scripts/Python/seeding.py
@@ -393,7 +393,7 @@ def runSeeding(trackingGeometry, field, outputDir, s=None):
         ),
         acts.logging.VERBOSE,
         geoSelectionConfigFile=srcdir
-        / "Examples/Algorithms/TrackFinding/share/geoSelection-genericDetector.json",
+        / "Examples/Algorithms/TrackFinding/share/geoSelection-openDataDetector.json",
         inputParticles="particles_final",  # use this to reproduce the original root_file_hashes.txt - remove to fix
         outputDirRoot=outputDir,
     )
