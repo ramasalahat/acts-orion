f9229398368fce01632e366a62cb9ff40e8e8db1
ramasalahat <ramasalahat@gmail.com>
2022-05-18 16:50:38 +0200

    experiment with plots

---
+++ Examples/Scripts/Python/generatePlot.py
@@ -0,0 +1,21 @@
+from orion.client import get_experiment
+import plotly.express as px
+
+# Specify the database where the experiments are stored. We use a local PickleDB here.
+storage = storage = {
+        "database": {
+            "type": "mongodb",
+            "name": 'orion_test',
+            "host": os.environ['mongodbbURI']
+        },
+    }
+
+# Load the data for the specified experiment
+experiment = get_experiment("newScoring", storage=storage)
+fig = experiment.plot.parallel_coordinates(colorscale="YlOrRd")
+fig.write_html("colored1.html")
+
+fig.update_traces(overwrite=True, color_continuous_scale="RdBu")
+fig.write_html("colored2.html")
+
+
