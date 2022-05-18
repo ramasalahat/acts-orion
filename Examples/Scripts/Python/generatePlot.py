c07cecbbccbfad92ac366d23efd77c27211c68c1
ramasalahat <ramasalahat@gmail.com>
2022-05-18 16:55:21 +0200

    mongodbURI fix

--- Examples/Scripts/Python/generatePlot.py
+++ Examples/Scripts/Python/generatePlot.py
@@ -3,13 +3,13 @@ if "__main__" == __name__:
     from orion.client import get_experiment
     import plotly.express as px
     import os
-    
+
     # Specify the database where the experiments are stored. We use a local PickleDB here.
     storage = storage = {
             "database": {
                 "type": "mongodb",
                 "name": 'orion_test',
-                "host": os.environ['mongodbbURI']
+                "host": os.environ['mongodbURI']
             },
         }
 
